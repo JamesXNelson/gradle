1df02a07a8020095c77f34ab5cc03e3b1304eccd
James X. Nelson <James@WeTheInter.net>
2019-05-21 03:16:30 -0600

    fix install script

--- install.sh
+++ install.sh
@@ -2,12 +2,16 @@
 set -e
 root=`dirname $(realpath $BASH_SOURCE)`
 # installs into local build/dist location that we control
-$root/gradlew installAll -x dslHtml -Pgradle_installPath=$root/build/dist/gradle-X $@
+$root/gradlew installAll -x dslHtml -x userguideSinglePage -Pgradle_installPath=$root/build/dist/gradle-X $@
 # upon success, move current contents out and new contents in.
 if [ -d "$root/build/dist/gradle-X" ]; then
-  rm -rf $root/build/dist/gradle-X-bak
   parent=`dirname $root`
+  echo "Removing $parent/gradle-X-bak"
+  rm -rf $parent/gradle-X-bak
+  echo "Backing up $parent/gradle-X"
   mv -f $parent/gradle-X $parent/gradle-X-bak > /dev/null 2>&1 || true
+  echo "Moving $root/build/dist/gradle-X to $parent"
   mv -f $root/build/dist/gradle-X $parent
 fi
+echo "Done"
 
